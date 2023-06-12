ba6e4d2bd4f3ed3f27f1fb9ab87fb822e6598551
belovetech <belovetech@gmail.com>
2023-06-12 09:42:36 +0100

    [Feat]: implement product struct and handler

--- go.mod
+++
@@ -1,8 +0,0 @@
-module go-rest-api/m
-
-go 1.20
-
-require (
-	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/mattn/go-sqlite3 v1.14.17 // indirect
-)
--- src/backend/backend.go
+++ src/backend/backend.go
@@ -2,9 +2,11 @@ package backend
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 	_ "github.com/mattn/go-sqlite3"
@@ -23,17 +25,63 @@ func (a *App) Initialize() {
 	}
 	a.DB = db
 	a.Router = mux.NewRouter()
-	a.initializeRouter()
+	a.initializeRoutes()
 }
 
-func (a *App) initializeRouter() {
+// Routers
+func (a *App) initializeRoutes() {
 	a.Router.HandleFunc("/", helloWorld).Methods("GET")
+	a.Router.HandleFunc("/products", a.allProducts).Methods("GET")
+	a.Router.HandleFunc("/product/{id}", a.fetchProduct).Methods("GET")
 }
 
+// Handlers
 func helloWorld(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello world!")
 }
 
+func (a *App) allProducts(w http.ResponseWriter, r *http.Request) {
+	products, err := getProducts(a.DB)
+
+	if err != nil {
+		log.Printf("Products error: %s", err.Error())
+		responseWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	responseWithJson(w, http.StatusOK, products)
+}
+
+func (a *App) fetchProduct(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	var p product
+	p.ID, _ = strconv.Atoi(id)
+
+	err := p.getProduct(a.DB)
+
+	if err != nil {
+		log.Printf("Products error: %s", err.Error())
+		responseWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	responseWithJson(w, http.StatusOK, p)
+}
+
+// Helper function
+func responseWithError(w http.ResponseWriter, code int, message string) {
+	responseWithJson(w, code, map[string]string{"Error": message})
+}
+
+func responseWithJson(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+// Run server
 func (a *App) Run() {
 	fmt.Println("Server started and listening on localhost:", a.Port)
 	log.Fatal(http.ListenAndServe(a.Port, a.Router))
